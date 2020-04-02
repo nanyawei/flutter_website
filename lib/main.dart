f0ca59bf0052697f9ee5075588419233c74d01cf
wyn <weiyanan@quantibio.com>
2020-04-02 15:29:49 +0800

    入口文件页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
+import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_website/loading.dart';
+import 'package:flutter_website/router/application.dart';
+import 'package:flutter_website/router/routers.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_website/provider/index.dart';
 
-void main () => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     // 实例化路由
-    // final router = Router
+    final router = Router();
+    // 配置路由
+    Routes.configRoutes(router);
+    // 将路由设置成整个应用可以调用
+    Application.router = router;
 
-
-
-    return MaterialApp(
-      title: 'Website',
-      theme: ThemeData(
-        primaryColor: Colors.redAccent
-      ),
-      home: Loading()
+    // 管理多个共享数据类
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CurrentIndexProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+        ChangeNotifierProvider(create: (_) => ProductDetailProvider()),
+        ChangeNotifierProvider(create: (_) => WebSocketProvider())
+      ],
+      child: MaterialApp(
+          title: 'Website',
+          onGenerateRoute: Application.router.generator,
+          theme: ThemeData(primaryColor: Colors.redAccent),
+          home: Loading()
+      )
     );
   }
 }
