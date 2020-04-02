59820fb4fa5ba9c8a6c902baf7517ef7f2cafb59
wyn <weiyanan@quantibio.com>
2020-04-02 15:25:36 +0800

    样式

---
+++ lib/style/color.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class ProductColors {
+  static const Color bgColor = Color(0xFFFFFFFF);
+  // 分割线颜色
+  static const dividerLineColor = Color.fromRGBO(245, 245, 245, 1.0);
+  // 产品颜色类型
+  static const typeColor = Color.fromRGBO(182, 9, 9, 1.0);
+  // 产品卖点颜色
+  static const point = Color.fromRGBO(132, 95, 63, 1.0);
+}
